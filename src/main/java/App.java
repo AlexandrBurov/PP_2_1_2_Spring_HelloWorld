bd2d6700d3ac9a88f0ce106298a0bb56d5e58988
macbook <a.d.burov@mail.ru>
2022-10-15 16:41:43 +0400

    .

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,17 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println("Результат проверки бинов: "+ (bean == bean1));
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Результат проверки бинов: "+ (cat == cat1));
+
+       // applicationContext.close();
+
     }
 }
